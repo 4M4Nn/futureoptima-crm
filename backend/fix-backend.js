9dc88f75e49d3a82dfc4f2b574741c0352dc3f0d
Aman Faisal <amanfaisals2001@gmail.com>
2026-06-23 13:28:58 +0530

    Remove test files with secrets

--- backend/fix-backend.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/routes/leads.js', 'utf8');
-c = c.replace(
-  "body('phone').matches(/^[6-9]\\d{9}$/),",
-  "body('phone').notEmpty().trim(),"
-);
-fs.writeFileSync('src/routes/leads.js', c);
-console.log('Fixed phone validation');
--- backend/fix-website.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-let env = fs.readFileSync('.env', 'utf8');
-env = env.replace(/INSTITUTE_WEBSITE=.*/g, 'INSTITUTE_WEBSITE=https://www.futureoptimaitsolutions.com');
-fs.writeFileSync('.env', env);
-console.log('Website URL updated!');
