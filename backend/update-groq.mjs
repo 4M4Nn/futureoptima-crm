7eafab4c8ea502f90b8aa05ad7146097d4ea64dd
Aman Faisal <amanfaisals2001@gmail.com>
2026-06-27 15:46:51 +0530

    Remove files with secrets

--- backend/update-groq.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-let env = fs.readFileSync('.env', 'utf8');
-env = env.replace(/GROQ_API_KEY=.*/g, 'GROQ_API_KEY=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');
-fs.writeFileSync('.env', env);
-console.log('GROQ API key updated in .env!');
